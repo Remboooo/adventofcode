389301277fee37a306f67bf6a6d16458d58730c4
Rembrand van Lakwijk <rembrand.vanlakwijk@nedap.com>
2020-12-02 08:55:18 +0100

    Add 2020 day 2

---
+++ 2020/2-1.py
@@ -0,0 +1,32 @@
+import re
+from argparse import ArgumentParser
+from itertools import combinations
+
+pass_re = re.compile(r'^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)$')
+
+
+def get_valid_passwords(lines):
+    for line in lines:
+        match = pass_re.fullmatch(line)
+        if not match:
+            raise ValueError(f"Corrupt line: {line}")
+        lo, hi = int(match[1]), int(match[2])
+        required = match[3]
+        password = match[4]
+
+        if lo <= len([1 for letter in password if letter == required]) <= hi:
+            yield password
+
+
+def main():
+    argparse = ArgumentParser()
+    argparse.add_argument("file", nargs='?', type=str, default="2-input.txt")
+    args = argparse.parse_args()
+    with open(args.file, "r") as f:
+        valid = list(get_valid_passwords([l.strip() for l in f]))
+        print(valid)
+        print(len(valid))
+
+
+if __name__ == '__main__':
+    main()
