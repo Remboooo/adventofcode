bccf385900eda675a37ec9930d9cb01b95913b11
Rembrand van Lakwijk <rembrand.vanlakwijk@blik-sensing.nl>
2020-12-06 11:10:44 +0100

    Add 2020 day 6

---
+++ 2020/6-1.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+from functools import reduce
+
+
+def read_groups(f):
+    return [[set(line) for line in group.split('\n') if line] for group in f.read().split('\n\n')]
+
+
+def get_any_yes_per_group(groups):
+    for group in groups:
+        yield reduce(lambda a, b: a | b, group, set())
+
+
+def main():
+    argparse = ArgumentParser()
+    argparse.add_argument("file", nargs='?', type=str, default="6-input.txt")
+    args = argparse.parse_args()
+    with open(args.file, "r") as f:
+        groups = read_groups(f)
+        groups_or = list(get_any_yes_per_group(groups))
+        print(sum(len(group) for group in groups_or))
+
+
+if __name__ == '__main__':
+    main()
