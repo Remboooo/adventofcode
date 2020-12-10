a5710178c4e8e2a95c32c8fad3e458f7dd67f20a
Rembrand van Lakwijk <rembrand.vanlakwijk@nedap.com>
2020-12-10 10:48:01 +0100

    Add forgotten util with memoized()

--- 2020/util.py
+++ 2020/util.py
@@ -24,3 +24,38 @@ def timed(func):
             print(f"{func_name(func)}() runtime: {time.monotonic() - start:.3f}s")
             return result
         return wrapper
+
+
+def _transform_arg(arg):
+    if isinstance(arg, set):
+        return frozenset(arg)
+    elif isinstance(arg, dict):
+        return tuple(sorted(arg.items()))
+    return arg
+
+
+def memoized(*key_args):
+    """
+    Memo-ize the given function using the given argument list as key.
+    The given argument list should contain argument numbers (for args) and/or names (for kwargs).
+    Only hashable types can be included in the argument list.
+    Generator functions cannot be memoized.
+    :param key_args: The arguments of the function to memoize
+    :return: Memoized version of the function
+    """
+    def wrapper_wrapper(func):
+        if inspect.isgeneratorfunction(func):
+            raise ValueError("Cannot memoize generator function")
+        else:
+            cache = {}
+
+            def memoizer(*args, **kwargs):
+                key = tuple(args[a] if isinstance(a, int) else kwargs[a] for a in key_args)
+
+                if key not in cache:
+                    cache[key] = func(*args, **kwargs)
+                return cache[key]
+
+            return memoizer
+
+    return wrapper_wrapper
