fcc8ba2dec5abb5863f331a4c98b3040d38dd55a
Rembrand van Lakwijk <rembrand.vanlakwijk@nedap.com>
2020-12-14 10:38:55 +0100

    Add 2020 day 13

--- 2020/13-2.py
+++ 2020/13-2.py
@@ -1,38 +1,35 @@
 from argparse import ArgumentParser
 
-from util import timed
+from util import timed, egcd
 
 
-def least_common_multiple_with_offset(a, b):
-    """
-    Calculates the least common multiple with an offset of 1, i.e.:
-    x = n*a+1 = m*b
-
-    According to https://cs.stackexchange.com/questions/83689/how-to-calculate-least-common-multiple-lcm-for-two-numbers-with-constants-shi
-    any solution must satisfy:
-    n = -1 / a (mod b)
-    and
-    m = 1 / b (mod a)
-    """
-    inva = modinv(a, b)
-    print(f"{a}^-1 mod {b} = {inva}")
-    n = (-1 * inva) % b
-
-    invb = modinv(b, a)
-    print(f"{b}^-1 mod {a} = {invb}")
-    m = (-1 * invb) % a
-
-    print(f"{n} = -1 / {a} (mod {b})")
-    print(f"{m} = -1 / {b} (mod {a})")
-    print(f"{n} * {a} + 1 = {n*a+1}")
-    print(f"{m} * {b} = {m * b}")
+def least_common_multiple_with_offset(a_period, a_offset, b_period, b_offset):
+    a_offset = (-a_offset) % a_period
+    b_offset = (-b_offset) % b_period
+    gcd, s, t = egcd(a_period, b_period)
+    pd_mult, pd_remainder = divmod(a_offset - b_offset, gcd)
+    combined_period = a_period // gcd * b_period
+    combined_phase = (a_offset - s * pd_mult * a_period) % combined_period
+    return -combined_phase % combined_period, combined_period
 
 
-def modinv(x, p):
-    """
-    Calculates the modular inverse; x^-1 mod p
-    """
-    return pow(x, -1, p)
+@timed
+def get_earliest_consecutive_departure(bus_ids):
+    remaining_bus_ids = bus_ids[1:]
+    total_offset = 0
+    total_period = bus_ids[0]
+    bus_offset = 0
+
+    while remaining_bus_ids:
+        bus_id = remaining_bus_ids[0]
+        remaining_bus_ids = remaining_bus_ids[1:]
+        bus_offset += 1
+        if bus_id is not None:
+            total_offset, total_period = least_common_multiple_with_offset(
+                total_period, total_offset, bus_id, -bus_offset
+            )
+
+    return total_offset
 
 
 def main():
@@ -40,10 +37,10 @@ def main():
     argparse.add_argument("file", nargs='?', type=str, default="13-input.txt")
     args = argparse.parse_args()
     with open(args.file, "r") as f:
-        now = int(f.readline().strip())
-        bus_intervals = [int(w) for w in f.readline().strip().split(',') if w != 'x']
+        f.readline()
+        bus_ids = [int(w) if w != 'x' else None for w in f.readline().strip().split(',')]
 
-    least_common_multiple_with_offset(7, 13)
+    print(get_earliest_consecutive_departure(bus_ids))
 
 
 if __name__ == '__main__':
