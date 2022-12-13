5647b8eddd0a3c7781a9c21019f6f06f6edc09bd
Rembrand van Lakwijk <rem@lakwijk.com>
2022-12-14 00:02:03 +0100

    Scratch the itch of inefficient 2022 day 12 part 2

--- 2022/src/12-2.kt
+++ 2022/src/12-2.kt
@@ -26,27 +26,26 @@ fun main(args: Array<String>) {
     val bounds = heights.bounds()
     val end = lines.coordsOf('E').first()
 
-    // This would be MUCH more efficient if we'd just find the shortest route from the end
-    // to the nearest point of height 0, but this is quick enough so ship it ¯\_(ツ)_/¯
-    val minDistance = heights.coordsOf(0).minOf { start ->
-        val distances = heights.map { Int.MAX_VALUE }
-        distances[start] = 0
+    val distances = heights.map { Int.MAX_VALUE }
+    distances[end] = 0
 
-        // Dijkstra that shit
-        val q = mutableListOf(start)
+    // Dijkstra that shit
+    val nearest = fun(): XY? {
+        val q = mutableListOf(end)
         while (q.isNotEmpty()) {
             val xy = q.removeFirst()
             val dist = distances[xy]
-            val neighbours = xy.neighbours(bounds).filter { heights[it] <= heights[xy] + 1 }
+            val neighbours = xy.neighbours(bounds).filter { heights[it] >= heights[xy] - 1 }
             for (neighbour in neighbours) {
                 if (distances[neighbour] > dist + 1) {
                     distances[neighbour] = dist + 1
+                    if (heights[neighbour] == 0) return neighbour
                     q.add(neighbour)
                 }
             }
         }
-        distances[end]
-    }
+        return null
+    }()!!
 
-    println(minDistance)
+    println(distances[nearest])
 }
